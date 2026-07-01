008f1245f2cf3d1bbb56fd1c91dd8350a34f0e52
Hythera <87016780+Hythera@users.noreply.github.com>
2026-07-01 16:16:10 +0200

    treewide: refactor

---
+++ modules/services.nix
@@ -0,0 +1,20 @@
+{
+  # Desktop
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.printing.enable = true;
+
+  # Audio
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  services.openssh.enable = true;
+}
