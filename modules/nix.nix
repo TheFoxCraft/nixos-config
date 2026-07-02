d26b61605dccd23f0b73386f7719a25444652fea
TheFoxCraft <thefoxcraft128@gmail.com>
2026-07-02 15:41:13 +0200

    feat: add garbage collector

--- modules/nix.nix
+++ modules/nix.nix
@@ -6,6 +6,12 @@
     "flakes"
   ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
