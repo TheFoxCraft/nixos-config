ad9a374b4f102e33f814019e9d5f70b3f708e0aa
TheFoxCraft <thefoxcraft128@gmail.com>
2026-07-02 15:41:41 +0200

    feat: add and sort the programs i use

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    trcc-linux = {
+      url = "github:TheFoxCraft/thermalright-trcc-linux?ref=fix/udev-rule";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
--- modules/packages.nix
+++ modules/packages.nix
@@ -1,15 +1,65 @@
 { inputs, pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-
-    element-desktop
-    git
-    signal-desktop
-    thunderbird
-    vesktop
-    vscodium
+  imports = [
+    inputs.trcc-linux.nixosModules.default
   ];
+
+  environment.systemPackages =
+    with pkgs;
+    (
+      [
+        inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+      ]
+
+      # development
+      ++ [
+        git
+        vscodium
+
+        nodejs
+
+        mockoon
+        insomnia
+        pgadmin4
+
+        nixfmt
+        openssl
+      ]
+
+      # communication
+      ++ [
+        element-desktop
+        signal-desktop
+        thunderbird
+        vesktop
+      ]
+
+      # gaming
+      ++ [
+        modrinth-app
+      ]
+
+      # tools
+      ++ [
+        btop
+        streamcontroller
+        rustdesk
+      ]
+
+      # office
+      ++ [
+        p3x-onenote
+      ]
+
+    );
+
+  virtualisation.docker.enable = true;
+
+  programs.nix-ld.enable = true;
+
+  programs.steam.enable = true;
+
+  programs.trcc-linux.enable = true;
 
   programs.zsh = {
     enable = true;
